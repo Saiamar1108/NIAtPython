30509f82a8f239f03898fa87db9331ee0f29510f
A.Sai Amar Chaitanya <saiamar110869@gmail.com>
2025-09-04 14:08:17 +0530

    Add pattern printing scripts for diamond, M, and triangle
    Added three new Python scripts in the problemsolving directory: diamong.py for printing a diamond pattern, mpattern.py for printing an 'M' shaped pattern, and triangle.py for printing a triangle pattern. Each script takes an integer input and prints the corresponding pattern.

---
+++ problemsolving/mpattern.py
@@ -0,0 +1,6 @@
+n=int(input())
+
+
+for i in range(1,n+1):
+  space =(2*(n-i))
+  print((" *"*i)+("  "*space)+(" *"*i))
---
+++ problemsolving/triangle.py
@@ -0,0 +1,7 @@
+n=int(input())
+
+
+for i in range(1,n+1):
+  space =((n-i))
+  print(("   "*space)+(" * "*(2*(i)-1))+("   "*space))
+ 
